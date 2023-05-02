fe89a892761b52c764ec96d35ab1a8063b91fe60
HellCounT <a_killaheadz@live.ru>
2023-05-02 21:13:45 +0300

    h17: users repo wip

--- test/e2e/superadmin/sa.users.e2e-spec.ts
+++ test/e2e/superadmin/sa.users.e2e-spec.ts
@@ -1,6 +1,10 @@
 import { INestApplication } from '@nestjs/common';
 import { setConfigNestApp } from '../../configuration.test';
 import request from 'supertest';
+import { superAdminUsersPath } from '../../helpers/paths';
+import { usersFactory } from '../../helpers/factory';
+import { superAdminLogin, superAdminPassword } from '../../helpers/auth';
+import { UserPaginatorType } from '../../../src/users/types/users.types';
 
 describe('Super Admin Blogs Controller (e2e)', () => {
   let nestApp: INestApplication;
@@ -18,5 +22,34 @@ describe('Super Admin Blogs Controller (e2e)', () => {
 
   beforeEach(async () => {
     await request(app).delete('/testing/all-data');
+  });
+
+  describe(`1. GET ${superAdminUsersPath}:`, () => {
+    it('1.1 Should return 401 if Super Admin credentials are not entered:', async () => {
+      await request(app).get(superAdminUsersPath).expect(401);
+    });
+    it('1.2. Should return 200 and paginated created user if correct credentials are entered:', async () => {
+      const user = usersFactory.createUser();
+      const userId = await usersFactory.insertUser(app, user);
+
+      const response = await request(app)
+        .get(superAdminUsersPath)
+        .auth(superAdminLogin, superAdminPassword)
+        .expect(200);
+      expect(response.body).toEqual<UserPaginatorType>({
+        pagesCount: 1,
+        page: 1,
+        pageSize: 10,
+        totalCount: 1,
+        items: [
+          {
+            id: userId,
+            login: user.login,
+            email: user.email,
+            createdAt: expect.any(String),
+          },
+        ],
+      });
+    });
   });
 });
