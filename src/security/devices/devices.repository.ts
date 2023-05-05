14c78449f1648661155f539d2c58cd48dca329e0
HellCounT <a_killaheadz@live.ru>
2023-05-05 18:51:06 +0300

    h17: sg

--- src/security/devices/devices.repository.ts
+++ src/security/devices/devices.repository.ts
@@ -37,6 +37,7 @@ export class DevicesRepository {
     }
   }
   async addSessionToDb(newSession: DeviceDb): Promise<void> {
+    console.log('new session: ', newSession);
     await this.dataSource.query(
       `
 INSERT INTO "DEVICES"
@@ -53,6 +54,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7)
         newSession.refreshTokenMeta,
       ],
     );
+    console.log('Session has started');
     return;
   }
   async updateSessionWithDeviceId(
